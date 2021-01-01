f520593894469d1151cd83f598db03cf7b330c7f
Yeon <yeon.yi@bigpond.com>
2021-01-02 01:42:59 +1100

    added local setting

--- btre/settings.py
+++ btre/settings.py
@@ -161,3 +161,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='yeonsapps@gmail.com'
 EMAIL_HOST_PASSWORD='$Adhanna5'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
